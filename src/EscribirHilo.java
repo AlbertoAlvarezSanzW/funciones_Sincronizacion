ad0cc5144b873a678606bccd9edfc8f39e99ec9a
Alberto <alberto.alvarez.sanz@gmail.com>
2020-11-08 17:52:48 +0100

    comit

--- src/EscribirHilo.java
+++ src/EscribirHilo.java
@@ -3,6 +3,7 @@ public class EscribirHilo extends Thread{
 
     @Override
     public void run() {
+        // llamada a la la clase principal con el metodo ejecución
         Main.Ejecucion(getName());
     }
 }
